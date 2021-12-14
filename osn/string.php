34abf8a28d3d3757520ff231f982224a92926e72
Krzysztof Wawoczny <k.wawoczny@gmi.pl>
2021-12-14 09:30:15 +0100

    Create string.php

---
+++ osn/string.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Use PHP documentation for the task.
+ *
+ * Display each operation on the screen.
+ * Perform the operations on the string assigned to $basicString .
+ *
+ * Hint: You can combine built-in PHP language functions.
+ */
+
+$basicString = ' AbcdEfGhjiKLMNX9 qrtstyAwp12x  ';
+echo 'Basic string: ';
+var_dump($basicString);
+echo '<br><br>';
+
+/** Convert characters to lowercase */
+echo strtolower($basicString);
+echo '<br><br>';
+
+//Follow the rest of the instructions
+
+/** Convert characters to uppercase */
+
+/** Convert characters to lowercase */
+
+/** Removes whitespace from the right end of a string */
+
+/** Removes whitespace from the left and right side of a string */
+
+/** Converts the first letter (not space) of a string to lowercase */
+
+/** Split a string into two strings, the second string must start with 9 */
+
+/** Combine two strings into one */
+
+/** At the beginning of the string, add <h1>, at the end of the string </h1> */
+
+/** Complete the string to 50 characters long by adding "_" to the end */
+
+/** Complete the string to 50 characters long by adding "_" to the beginning */
+
+/** Replace all occurrences of "A" with "@" */
+
+/** Shuffle all characters in the string */
+
+/** Count the characters */
+
+/** Count words in string */
+
+/** Replace the string with a random string */
+
+/** Replace a string with an array where each element is a single character */
+
+/** Rewrite the string in reverse order */
+
+/** Where is "G" in the string ? (Which item number) */
+
+/** Duplicate the string twice */
