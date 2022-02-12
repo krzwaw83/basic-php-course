e11ef7ecb66252114c996db4f0af72f9d2b0426f
Marek Wawoczny <wawocznym@gmail.com>
2022-02-12 20:52:10 +0100

    Updates / Solutions

--- osn/numbers.php
+++ osn/numbers.php
@@ -16,6 +16,10 @@ echo 'Basic number: ';
 var_dump($basicNumber);
 echo '<br><br>';
 
+echo 'Basic number float: ';
+var_dump($basicFloat);
+echo '<br><br>';
+
 /** Generate random number */
 echo rand();
 echo '<br><br>';
@@ -24,32 +28,107 @@ echo '<br><br>';
 
 /** Increment $basicNumber by 1 */
 
+echo 'Basic number increment by 1 : ';
+$basicNumber++;
+echo $basicNumber;
+echo '<br><br>';
+
+
 /** Decrement $basicNumber by 1 */
+
+echo 'Basic number decrement by 1 : ';
+
+$basicNumber = 123456;
+$basicNumber--;
+echo $basicNumber;
+echo '<br><br>';
 
 /** Convert $basicNumber to a floating point number with two decimal places - expected 123456.00 */
 
+echo 'Convert $basicNumber to a floating point number with two decimal places :';
+$basicNumber = 123456;
+var_dump((float)number_format($basicNumber, 2, '.', ''));
+echo '<br><br>';
+
 /** Convert $basicFloat to integer */
+
+echo 'Convert $basicFloat to integer  :';
+echo intval($basicFloat);
+echo '<br><br>';
+
 
 /** Convert $basicFloat to boolean */
 
+echo 'Convert $basicFloat to boolean  :';
+echo boolval($basicFloat);
+echo '<br><br>';
+
 /** Converts $basicFloat to a floating point number with two decimal places - expected: 123.45 */
+
+echo 'Converts $basicFloat to a floating point number with two decimal places - expected: 123.45  :';
+var_dump(floor(round($basicFloat,2)));
+echo '<br><br>';
 
 /** Converts $basicFloat to a floating point number with two decimal places - expected: 123.46 */
 
+echo 'Converts $basicFloat to a floating point number with two decimal places - expected: 123.46  :';
+var_dump(round($basicFloat,2));
+echo '<br><br>';
+
 /** Sum $basicFloat and $basicNumber */
+
+echo 'Sum $basicFloat and $basicNumber  :';
+$sum = $basicFloat + $basicNumber;
+echo $sum;
+echo '<br><br>';
 
 /** Do: $basicFloat - $basicNumber. Show results as a positive number */
 
+echo 'Sum $basicFloat and $basicNumber  :';
+$subtraction = abs($basicFloat - $basicNumber);
+echo $subtraction;
+echo '<br><br>';
+
 /** Pre-Increment $basicNumber (operator ++) */
+
+echo 'Pre-Increment $basicNumber (operator ++)  :';
+echo " Should be 123457: " . ++$basicNumber . "<br />\n";
 
 /** Pre-Decrement $basicFloat */
 
+$basicFloat  = 123.456;
+echo 'Pre-Decrement $basicFloat  :';
+echo " Should be 122.456 : " . --$basicFloat . "<br />\n";
+
 /** Post-Increment $basicNumber */
+
+echo 'Post-Increment $basicNumber  :';
+$basicNumber = 123456;
+echo " First should be (123456) : " . $basicNumber++ . " then should be (123457) : " . $basicNumber . "<br />\n";
 
 /** Post-Decrement $basicFloat */
 
+echo 'Post-Decrement $basicFloat  :';
+$basicFloat  = 123.456;
+echo " First should be (123.456) : " . $basicFloat-- . " then should be (122.456) : " . $basicFloat . "<br />\n";
+
 /** Raise a $basicNumber to the power of two (a^2 - but do not use ^) */
+
+echo 'Raise a $basicNumber to the power of two (a^2 - but do not use ^)  :';
+$basicNumber = 123456;
+echo pow($basicNumber, 2);
+echo '<br><br>';
 
 /**  Round fractions of $basicFloat down. Expected: 123 */
 
+$basicFloat  = 123.456;
+echo "Round fractions of $ basicFloat down. Expected: 123 -> ";
+var_dump(floor($basicFloat));
+echo '<br><br>';
+
 /**  Round fractions of $basicFloat up. Expected: 124 */
+
+$basicFloat  = 123.456;
+echo "Round fractions of $ basicFloat up. Expected: 124 -> ";
+var_dump(ceil($basicFloat));
+echo '<br><br>';
--- osn/string.php
+++ osn/string.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Use PHP documentation for the task.
  *
@@ -14,46 +13,149 @@ echo 'Basic string: ';
 var_dump($basicString);
 echo '<br><br>';
 
+
+/** Convert characters to uppercase */
+echo '1) String in uppercase => ';
+echo strtoupper($basicString);
+echo '<br><br>';
+
 /** Convert characters to lowercase */
+echo 'String in lowercase => ';
 echo strtolower($basicString);
 echo '<br><br>';
 
-//Follow the rest of the instructions
-
-/** Convert characters to uppercase */
-
-/** Convert characters to lowercase */
-
 /** Removes whitespace from the right end of a string */
+
+echo '2) Removes whitespace from the right end of a string => ';
+var_dump (rtrim($basicString));
+echo '<br><br>';
 
 /** Removes whitespace from the left and right side of a string */
 
+echo '3) Removes whitespace from the left and right side of a string => ';
+var_dump (trim($basicString));
+echo '<br><br>';
+
 /** Converts the first letter (not space) of a string to lowercase */
+
+echo '4) Converts the first letter (not space) of a string to lowercase => ';
+$basicString = ltrim($basicString); // Remove space from the beginning (left side)
+$basicString = lcfirst($basicString);
+$basicString = ' ' . $basicString;
+var_dump ($basicString);
+echo '<br><br>';
 
 /** Split a string into two strings, the second string must start with 9 */
 
+echo '5) Split a string into two strings, the second string must start with 9 => ';
+echo '<br><br>';
+$basicString = ' AbcdEfGhjiKLMNX9 qrtstyAwp12x ';
+$firstBasicString = substr($basicString, 0, 16);
+$secondBasicString = strchr($basicString, "9");
+echo 'First String =>' . $firstBasicString;
+echo '<br><br>';
+echo 'Second string start with 9 => ' . $secondBasicString;
+echo '<br><br>';
+
 /** Combine two strings into one */
+
+echo '6) Combine two string into one => ';
+$result = $firstBasicString . $secondBasicString;
+echo '<br><br>';
+echo 'First string =>' . ' ' . $firstBasicString;
+echo '<br><br>';
+echo 'Second string =>' . ' ' . $secondBasicString;
+echo '<br><br>';
+echo 'Two strings into one I methode =>' . ' ' . $result;
+echo '<br><br>';
+echo 'Two strings into one II methode =>';
+echo printf("%s%s", $firstBasicString, $secondBasicString);
+echo '<br><br>';
+
 
 /** At the beginning of the string, add <h1>, at the end of the string </h1> */
 
+$basicString = ' AbcdEfGhjiKLMNX9 qrtstyAwp12x  ';
+$prefix = "<h1>";
+$suffix = "</h1>";
+echo '7) At the beginning of the string, add < h1 > , at the end of the string < /h1 > => ';
+echo '<br><br>';
+echo htmlspecialchars($prefix . $basicString . $suffix);
+echo '<br><br>';
+
 /** Complete the string to 50 characters long by adding "_" to the end */
 
+echo '8) Complete the string to 50 characters long by adding "_" to the end => ';
+echo str_pad($basicString, 50, "_", STR_PAD_RIGHT);
+echo '<br><br>';
+
+
 /** Complete the string to 50 characters long by adding "_" to the beginning */
+echo '9) Complete the string to 50 characters long by adding "_" to the beginning => ';
+echo str_pad($basicString, 50, "_", STR_PAD_LEFT);
+echo '<br><br>';
 
 /** Replace all occurrences of "A" with "@" */
 
+echo '10) Replace all occurrences of "A" with "@" => ';
+echo str_replace("A","@",$basicString);
+echo '<br><br>';
+
 /** Shuffle all characters in the string */
+
+echo '11) Shuffle all characters in the string  => ';
+echo str_shuffle($basicString);
+echo '<br><br>';
 
 /** Count the characters */
 
+echo '12) Count the characters => ';
+echo strlen($basicString);
+echo '<br><br>';
+
 /** Count words in string */
+
+echo '13) Count words in string => ';
+echo str_word_count($basicString);
+echo '<br><br>';
 
 /** Replace the string with a random string */
 
+echo '14) Replace the string with a random string => ';
+echo str_replace($basicString, str_shuffle($basicString), $basicString);
+echo '<br><br>';
+
 /** Replace a string with an array where each element is a single character */
+
+echo '15) Replace a string with an array where each element is a single character => ';
+$basicString = ' AbcdEfGhjiKLMNX9 qrtstyAwp12x  ';
+var_dump (str_split($basicString));
+echo '<br><br>';
 
 /** Rewrite the string in reverse order */
 
+echo '16) Rewrite the string in reverse order => ';
+echo strrev($basicString);
+echo '<br><br>';
+
 /** Where is "G" in the string ? (Which item number) */
 
+echo '17) Where is "G" in the string ? (Which item number) => ';
+$basicString = ' AbcdEfGhjiKLMNX9 qrtstyAwp12x  ';
+$findme   = 'G';
+$pos = strpos($basicString, $findme);
+
+if ($pos === false) {
+    echo "The string '$findme' was not found in the string '$basicString'";
+} else {
+    echo "The string '$findme' was found in the string '$basicString'";
+    echo " and exists at position $pos";
+}
+echo '<br><br>';
+
 /** Duplicate the string twice */
+
+echo '18) Duplicate the string twice => ';
+echo str_repeat($basicString, 2);
+echo '<br><br>';
+
